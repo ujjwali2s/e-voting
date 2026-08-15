67ed7b3a5b8db4955a2d26a902bf8ccb48ec118c
ujjwali2s <ujjwalis493@gmail.com>
2026-08-15 12:53:19 +0530

    oh

--- e_voting/settings.py
+++ e_voting/settings.py
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '%6lp_p!%r$7t-2ql5hc5(r@)8u_fc+6@ugxcnz=h=b(fn#3$p9'
+SECRET_KEY = os.getenv('SECRET_KEY', '%6lp_p!%r$7t-2ql5hc5(r@)8u_fc+6@ugxcnz=h=b(fn#3$p9')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'True').lower() in ('true', '1', 't')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', '*').split(',')
 
 
 # Application definition
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -86,13 +87,18 @@ WSGI_APPLICATION = 'e_voting.wsgi.application'
 # Developed by: Ravi Ranjan Kumar
 # ============================================================
 
+# Default host logic:
+# On Render internal network, default to internal host 'dpg-da00t9tbedkc739klqg0-a'
+# Otherwise, default to external host 'dpg-da00t9tbedkc739klqg0-a.oregon-postgres.render.com'
+default_db_host = 'dpg-da00t9tbedkc739klqg0-a' if os.getenv('RENDER') else 'dpg-da00t9tbedkc739klqg0-a.oregon-postgres.render.com'
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': os.getenv('DB_NAME', 'evoting_db_2xky'),
         'USER': os.getenv('DB_USER', 'evoting_user'),
         'PASSWORD': os.getenv('DB_PASSWORD', 'PkxCPqq9zZg4VEb8zUVv1RGZHlyNUy4U'),
-        'HOST': os.getenv('DB_HOST', 'dpg-da00t9tbedkc739klqg0-a.oregon-postgres.render.com'),
+        'HOST': os.getenv('DB_HOST', default_db_host),
         'PORT': os.getenv('DB_PORT', '5432'),
         'OPTIONS': {
             'sslmode': os.getenv('DB_SSLMODE', 'require'),
@@ -140,13 +146,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static')
-]
 
 AUTH_USER_MODEL = 'account.CustomUser'
 AUTHENTICATION_BACKENDS = ['account.email_backend.EmailBackend']
